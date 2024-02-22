aa81e119b666cb95465029fbca8ca9b8bd3e5af7
Eric Mogendi <mogendieric19@gmail.com>
2024-02-23 02:04:05 +0300

    second commit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,30 +1,36 @@
-const { readFile, writeFile } = require("fs");
-console.log('start')
+const { readFile, writeFile } = require('fs').promises;
 
-readFile("./content/first.txt", "utf8", (err, result) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	const first = result;
-	readFile("./content/second.txt", "utf8", (err, result) => {
-		if (err) {
-			console.log(err);
-			return;
-		}
-		const second = result;
-		writeFile(
-			"./content/result-async.txt",
-			`Here is the result: ${first}, ${second}`,
-			(err, result) => {
-				if (err) {
-					console.log(err);
-					return;
-				}
-				console.log('Done with this task');
-			}
+const start = async () => {
+	try {
+		const first = await readFile('./content/first.txt', 'utf8');
+		const second = await readFile('./content/second.txt', 'utf8');
+		await writeFile(
+			'./content/result-mind-grenade.tx',
+			`This is awesome : ${first} ${second}`
 		);
-	});
-});
+		console.log(first, second);
+	} catch (error) {
+		console.log(error);
+	}
+};
+start();
 
-console.log('starting next task')
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
+
+// const getText = (path) => {
+// 	return new Promise((resolve, reject) => {
+// 		readFile(path, 'utf-8', (err, data) => {
+// 			if (err) {
+// 				reject (err);
+// 			} else {
+// 				resolve(data);
+// 			}
+// 		});
+// 	});
+// };
+
+// getText('./content/first.txt')
+// 	.then((result) => console.log(result))
+// 	.catch((err) => console.log(err));
